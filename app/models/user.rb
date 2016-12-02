513e1b45273c8d3db98b47d71d29834f951d943b
Pea M. Tyczynska <CrystalPea@github.com>
2016-12-02 12:27:47 +0000

    Added passwor encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require 'bcrypt'
 
 class User
+  include BCrypt
   include DataMapper::Resource
   property :id, Serial
   property :email, String
-  property :password, String
+  property :cryptpass, Text
 
+  def password=(password)
+    self.cryptpass = BCrypt::Password.create(password)
+  end
 end
